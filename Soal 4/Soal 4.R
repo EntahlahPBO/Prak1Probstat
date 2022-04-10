1cf1427ea528304458b80861ab74492613f3564e
Gery Febrian <92217354+EntahlahPBO@users.noreply.github.com>
2022-04-10 11:47:48 +0700

    Update Soal 4.R

--- Soal 4/Soal 4.R
+++ Soal 4/Soal 4.R
@@ -1,18 +1,17 @@
-#Inisialisasi nilai X dan V
+#Nomor 4.A
 x <- 2
 v <- 10
-#Distribusi Chi Square
 
 dchisq(x,v,ncp=0)
 
-#Histogram dari Distribusi Chi-Square dengan 100 data random.
+#Nomor 4.B
 set.seed(0)
 hist(rchisq(n,l),
      main="Histogram poison kelahiran bayi",
      xlim = c(0,20),
      col="azure",
 )
-#Nilai Rataan (??) dan Varian (??²) dari DistribusiChi-Square.
+#Nomor 4.C
 mu = v
 mu
 
